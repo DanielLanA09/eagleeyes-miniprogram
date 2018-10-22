2e2b727de51fe9ef367903f6d6a1e18dd7ce4444
Lan Jian <lanjian2012@live.com>
2018-10-22 10:26:48 +0800

    3.2

--- src/main/java/com/eagleshing/miniprogram/EagleEyeWxApplication.java
+++ src/main/java/com/eagleshing/miniprogram/EagleEyeWxApplication.java
@@ -3,15 +3,22 @@ package com.eagleshing.miniprogram;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 import com.eagleshing.miniprogram.property.FileStorageProperties;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @EnableConfigurationProperties({FileStorageProperties.class})
 @SpringBootApplication
 @MapperScan("com.eagleshing.miniprogram.domain.mappers")
-public class EagleEyeWxApplication {
+public class EagleEyeWxApplication extends SpringBootServletInitializer {
 	public static void main(String[] args) {
 		SpringApplication.run(EagleEyeWxApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(EagleEyeWxApplication.class);
 	}
 }
